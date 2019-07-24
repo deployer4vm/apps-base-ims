b578c2ede111682ccbfc13bb225a7b0389efa286
Harry Prayudhi <kang.harry@gmail.com>
2019-07-24 08:01:21 +0700

    fix

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\ViewServiceProvider::class,
         App\Providers\TranslationServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\MainApp\Providers\AppServiceProvider::class,
 
     ],
 
