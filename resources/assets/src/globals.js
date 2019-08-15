e3ec97889dd1ed53c3ee750dd5fe7eaffc65172b
Harry Prayudhi <kang.harry@gmail.com>
2019-08-15 09:40:38 +0700

    add download feature

--- resources/assets/src/globals.js
+++ resources/assets/src/globals.js
@@ -20,90 +20,127 @@ localapi.defaults.headers.common['Content-Type'] = 'multipart/form-data';
 jika multi tenant aktif
 */
 if(AppConfig.system.web_admin.multitenant.active){
-  var newEndpoint = {};
-  _.forEach(AppConfig.endpoint.admin,(v,i)=>{
-    newEndpoint[i] = "/:group_app" + v;
-  });
-  AppConfig.endpoint.admin = newEndpoint;
+    var newEndpoint = {};
+    _.forEach(AppConfig.endpoint.admin,(v,i)=>{
+        newEndpoint[i] = "/:group_app" + v;
+    });
+    AppConfig.endpoint.admin = newEndpoint;
 }
 
+/**
+ * Downloader
+ */
+let downloadVar = {
+    path: ''
+}
 export default function () {
-  return {
-    // Public url
-    publicUrl: '/',
-    
-    // Layout helpers
-    layoutHelpers,
+    return {
+        // Public url
+        publicUrl: '/',
+        
+        // Layout helpers
+        layoutHelpers,
 
-    //config app
-    AppConfig,
+        //config app
+        AppConfig,
 
-    //translation / locale
-    Trans,
-    LocalApi: localapi,
-    UserAuth,
-    
-    Web: web,
+        //translation / locale
+        Trans,
 
-    // Check for RTL layout
-    get isRTL () {
-      return document.documentElement.getAttribute('dir') === 'rtl' ||
-             document.body.getAttribute('dir') === 'rtl'
-    },
+        //local api
+        LocalApi: localapi,
 
-    // Check if IE
-    get isIEMode () {
-      return typeof document['documentMode'] === 'number'
-    },
+        //user auth helper
+        UserAuth,
 
-    // Check if IE10
-    get isIE10Mode () {
-      return this.isIEMode && document['documentMode'] === 10
-    },
+        //downloader
+        download: function(path,filename) {    
+            let docUrl = this.downloadVar.path + path;
+            axios({
+                method: 'get',
+                url: docUrl,
+                responseType: 'arraybuffer'
+            })
+            .then(response => { 
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', filename); //or any other extension
+                document.body.appendChild(link);
+                link.click();        
+            })
+            .catch((err) => {
+                console.log('download error : ',err);
+                web.showAlert({text: "Download Error",type: "warning"});
+            });
+        },
+        downloadVar: downloadVar,
+        setDownloadPath(path){
+            this.downloadVar.path = path;
+        },
+        
+        //general web helper
+        Web: web,
 
-    // Layout navbar color
-    get layoutNavbarBg () {
-      return this.AppConfig.system.web_admin.navbar_bgcolor;
-    },
+        // Check for RTL layout
+        get isRTL () {
+            return document.documentElement.getAttribute('dir') === 'rtl' ||
+            document.body.getAttribute('dir') === 'rtl'
+        },
 
-    // Layout sidenav color
-    get layoutSidenavBg () {
-      return this.AppConfig.system.web_admin.sidenav_bgcolor;
-    },
+        // Check if IE
+        get isIEMode () {
+            return typeof document['documentMode'] === 'number'
+        },
 
-    // Layout footer color
-    get layoutFooterBg () {
-      return this.AppConfig.system.web_admin.footer_bgcolor;
-    },
+        // Check if IE10
+        get isIE10Mode () {
+            return this.isIEMode && document['documentMode'] === 10
+        },
 
-    
-    // Animate scrollTop
-    scrollTop (to, duration, element = document.scrollingElement || document.documentElement) {
-      if (element.scrollTop === to) return
-      const start = element.scrollTop
-      const change = to - start
-      const startDate = +new Date()
+        // Layout navbar color
+        get layoutNavbarBg () {
+            return this.AppConfig.system.web_admin.navbar_bgcolor;
+        },
 
-      // t = current time; b = start value; c = change in value; d = duration
-      const easeInOutQuad = (t, b, c, d) => {
-        t /= d / 2
-        if (t < 1) return c / 2 * t * t + b
-        t--
-        return -c / 2 * (t * (t - 2) - 1) + b
-      }
+        // Layout sidenav color
+        get layoutSidenavBg () {
+            return this.AppConfig.system.web_admin.sidenav_bgcolor;
+        },
 
-      const animateScroll = () => {
-        const currentDate = +new Date()
-        const currentTime = currentDate - startDate
-        element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration))
-        if (currentTime < duration) {
-          requestAnimationFrame(animateScroll)
-        } else {
-          element.scrollTop = to
+        // Layout footer color
+        get layoutFooterBg () {
+            return this.AppConfig.system.web_admin.footer_bgcolor;
+        },
+
+        
+        // Animate scrollTop
+        scrollTop (to, duration, element = document.scrollingElement || document.documentElement) {
+            if (element.scrollTop === to) return
+            const start = element.scrollTop
+            const change = to - start
+            const startDate = +new Date()
+
+            // t = current time; b = start value; c = change in value; d = duration
+            const easeInOutQuad = (t, b, c, d) => {
+                t /= d / 2
+                if (t < 1) return c / 2 * t * t + b
+                t--
+                return -c / 2 * (t * (t - 2) - 1) + b
+            }
+
+            const animateScroll = () => {
+                const currentDate = +new Date()
+                const currentTime = currentDate - startDate
+                element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration))
+                if (currentTime < duration) {
+                    requestAnimationFrame(animateScroll)
+                } else {
+                    element.scrollTop = to
+                }
+            }
+
+            animateScroll()
         }
-      }
-
-      animateScroll()
     }
-  }
 }
