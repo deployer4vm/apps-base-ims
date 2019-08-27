3f0e8f9e137445fbb9a261eb1f94a77044716d69
Harry Prayudhi <kang.harry@gmail.com>
2019-08-27 15:57:08 +0700

    fix

--- app/Services/Excel.php
+++ app/Services/Excel.php
@@ -9,7 +9,37 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 class Excel
 {
     public $TBS;
+    public $letterMap = array(1=>'A',2=>'B',3=>'C',4=>'D',5=>'E',6=>'F',7=>'G'
+		,8=>'H',9=>'I',10=>'J',11=>'K',12=>'L',13=>'M',14=>'N',15=>'O',16=>'P',17=>'Q',18=>'R'
+        ,19=>'S',20=>'T',21=>'U',22=>'V',23=>'W',24=>'X',25=>'Y',26=>'Z');
 
+    /**
+	 * excel colom, ubah angka kolom ke kolom excel
+	 */
+	public function excol($int=false){
+		$hasil = array();
+		
+		while ($s1 = floor($int/26)){
+			$s2 = $int % 26;
+			if($s1>=1){
+				if(isset($this->letterMap[$s2])){
+					array_unshift($hasil,$this->letterMap[$s2]);
+					$int = $s1;
+				}else{
+					array_unshift($hasil,'Z');
+					$int = $s1-1;
+				}
+				
+			}else{
+				$int = 26;
+				break;
+			}
+		}
+		if(isset($this->letterMap[$int]))array_unshift($hasil,$this->letterMap[$int]);
+		
+		$hasil = implode('', $hasil);
+		return $hasil;
+	}
     public function load($template, $format = 'Xls')
 	{
         $filepathTemplate = app_path('MainApp/resources/doc/'.$template);
