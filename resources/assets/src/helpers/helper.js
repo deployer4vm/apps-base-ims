a0a28221ee5ea0e655b469693ead1c506337b36a
Harry Prayudhi <kang.harry@gmail.com>
2019-11-24 06:41:49 +0700

    update

--- resources/assets/src/helpers/helper.js
+++ resources/assets/src/helpers/helper.js
@@ -1,5 +1,5 @@
 /**
- * General global function
+ * General global function this.Helper / globals().Helper
  */
 export default {
     /**
@@ -14,5 +14,53 @@ export default {
         }
 
         return parameters.join('&');
+    },
+    //convert array (3 level) to FormData object
+    convertToFormData(form) {                   
+        let formData = new FormData();   
+        _.forEach(form,(v,k)=>{
+            if(v instanceof Object && !(v instanceof String)){
+                if(v instanceof File){
+                    formData.append(k, v);                                
+                }else{
+                    _.forEach(v,(v2,k2)=>{
+                        if(v2 instanceof Object && !(v2 instanceof String)){
+                            if(v2 instanceof File){
+                                formData.append(k+'['+k2+']', v2);                                
+                            }else{
+                                _.forEach(v2,(v3,k3)=>{   
+
+                                    if(v3 instanceof Object && !(v3 instanceof String)){
+                                        if(v3 instanceof File){
+
+                                            formData.append(k+'['+k2+']'+'['+k3+']', v3);                                
+                                        }else{
+                                            
+                                            _.forEach(v3,(v4,k4)=>{
+                                                if(v4!=null)
+                                                    formData.append(k+'['+k2+']'+'['+k3+']'+'['+k4+']', v4);
+                                            });
+                                        }
+                                    }else{
+                                        if(v3!=null)
+                                            formData.append(k+'['+k2+']'+'['+k3+']', v3);
+                                    }
+                                });
+                            }
+                        }else{
+                            if(v2!=null)
+                                formData.append(k+'['+k2+']', v2);
+                        }
+                        
+                    });
+
+                }
+            }else{
+                if(v!=null)
+                    formData.append(k, v);                            
+            }
+        });     
+        return formData;    
+        
     }
 }
