943924d6590abc36bbfc7562303a2185359b6e7e
Rijwan <rijwan@4visionmedia.com>
2022-04-18 15:43:30 +0700

    Update helper

--- app/Services/Helper.php
+++ app/Services/Helper.php
@@ -60,28 +60,28 @@ class Helper
     
     public function terbilang($nilai) {
 		$nilai = abs($nilai);
-		$huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+		$huruf = array("", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
 		$temp = "";
 		if ($nilai < 12) {
 			$temp = " ". $huruf[$nilai];
 		} else if ($nilai <20) {
-			$temp = $this->terbilang($nilai - 10). " belas";
+			$temp = $this->terbilang($nilai - 10). " Belas";
 		} else if ($nilai < 100) {
-			$temp = $this->terbilang($nilai/10)." puluh". $this->terbilang($nilai % 10);
+			$temp = $this->terbilang($nilai/10)." Puluh". $this->terbilang($nilai % 10);
 		} else if ($nilai < 200) {
-			$temp = " seratus" . $this->terbilang($nilai - 100);
+			$temp = " Seratus" . $this->terbilang($nilai - 100);
 		} else if ($nilai < 1000) {
-			$temp = $this->terbilang($nilai/100) . " ratus" . $this->terbilang($nilai % 100);
+			$temp = $this->terbilang($nilai/100) . " Ratus" . $this->terbilang($nilai % 100);
 		} else if ($nilai < 2000) {
-			$temp = " seribu" . $this->terbilang($nilai - 1000);
+			$temp = " Seribu" . $this->terbilang($nilai - 1000);
 		} else if ($nilai < 1000000) {
-			$temp = $this->terbilang($nilai/1000) . " ribu" . $this->terbilang($nilai % 1000);
+			$temp = $this->terbilang($nilai/1000) . " Ribu" . $this->terbilang($nilai % 1000);
 		} else if ($nilai < 1000000000) {
-			$temp = $this->terbilang($nilai/1000000) . " juta" . $this->terbilang($nilai % 1000000);
+			$temp = $this->terbilang($nilai/1000000) . " Juta" . $this->terbilang($nilai % 1000000);
 		} else if ($nilai < 1000000000000) {
-			$temp = $this->terbilang($nilai/1000000000) . " milyar" . $this->terbilang(fmod($nilai,1000000000));
+			$temp = $this->terbilang($nilai/1000000000) . " Milyar" . $this->terbilang(fmod($nilai,1000000000));
 		} else if ($nilai < 1000000000000000) {
-			$temp = $this->terbilang($nilai/1000000000000) . " trilyun" . $this->terbilang(fmod($nilai,1000000000000));
+			$temp = $this->terbilang($nilai/1000000000000) . " Trilyun" . $this->terbilang(fmod($nilai,1000000000000));
 		}     
 		return $temp;
 	}
